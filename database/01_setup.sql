079d9f8e61a880eec4f02bb2a9a2407462e7c788
mtte <kay.mattern@gibmit.ch>
2019-12-02 14:20:10 +0100

    Create the database schema

--- database/01_setup.sql
+++ database/01_setup.sql
@@ -1,4 +1,42 @@
-CREATE TABLE IF NOT EXISTS test (
-    id INT PRIMARY KEY NOT NULL,
-    test VARCHAR(255) NOT NULL
+-- SCRIPT TO CREATE THE DATABASE
+
+-- TABLE user
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id       SERIAL       NOT NULL,
+    name     VARCHAR(50)  NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- TABLE note
+CREATE TABLE IF NOT EXISTS public.note
+(
+    id      SERIAL       NOT NULL,
+    title   VARCHAR(100) NOT NULL,
+    content TEXT         NOT NULL,
+    created TIMESTAMP    NOT NULL,
+    author  INT          NOT NULL,
+    shared  BOOLEAN      NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (author) REFERENCES public."user" (id)
+);
+
+-- TABLE category
+CREATE TABLE IF NOT EXISTS public.category
+(
+    id   SERIAL       NOT NULL,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+-- TABLE note_has_category
+CREATE TABLE IF NOT EXISTS public.note_has_category
+(
+    note     INT NOT NULL,
+    category INT NOT NULL,
+    PRIMARY KEY (note, category),
+    FOREIGN KEY (note) REFERENCES public.note (id),
+    FOREIGN KEY (category) REFERENCES public.category (id)
 );
