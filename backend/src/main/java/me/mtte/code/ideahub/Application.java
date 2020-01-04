4332ef0677f2f2c3d08fdb9a250f2727d687bd37
mtte <kay.mattern@gibmit.ch>
2020-01-05 00:14:10 +0100

    Add CORS

--- backend/src/main/java/me/mtte/code/ideahub/Application.java
+++ backend/src/main/java/me/mtte/code/ideahub/Application.java
@@ -27,8 +27,6 @@ public class Application {
     }
 
     private static void setupApi() {
-        port(80);
-
         before((request, response) -> {
             // Remove trailing slashes of requests
             String path = request.pathInfo();
@@ -37,11 +35,37 @@ public class Application {
             }
         });
 
+        enableCORS("*", "*", "*");
+
         path("/api", new Api(database, securityConfig));
 
         after((req, res) -> {
             res.header("Content-Encoding", "gzip");
             res.type("application/json");
+        });
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
         });
     }
 
