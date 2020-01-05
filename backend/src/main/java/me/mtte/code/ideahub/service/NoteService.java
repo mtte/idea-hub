6d89fbef0c2c5906253a69265a45bd457c35a6e0
Simai00 <simon.friedli@gibmit.ch>
2020-01-05 19:50:01 +0100

    Commit

--- backend/src/main/java/me/mtte/code/ideahub/service/NoteService.java
+++ backend/src/main/java/me/mtte/code/ideahub/service/NoteService.java
@@ -36,7 +36,7 @@ public class NoteService extends AbstractService {
                 .join(USER)
                 .on(NOTE.AUTHOR.eq(USER.ID))
                 .where(NOTE.AUTHOR.eq(userId)
-                        .or(NOTE.SHARED))
+                        .or(NOTE.SHARED.eq(true)))
                 .fetch();
         return result.stream()
                 .map(r -> new Note(r.value1(), r.value2(), r.value3(), r.value4(), r.value5(), r.value6()))
