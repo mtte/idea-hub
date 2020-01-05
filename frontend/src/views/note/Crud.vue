56b8350e47117733e281d50d2dd2611794375541
Simai00 <simon.friedli@gibmit.ch>
2020-01-05 16:41:46 +0100

    Init Note Crud

---
+++ frontend/src/views/note/Crud.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <h1>Notiz</h1>
+    <form>
+      <v-text-field />
+    </form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'NoteCrud'
+}
+</script>
+
+<style scoped>
+
+</style>
