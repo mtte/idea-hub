aead320ba990a162e48cdf56af9225082e407d86
mtte <kay.mattern@gibmit.ch>
2020-01-05 17:22:38 +0100

    Return author name instead of id

--- backend/src/main/java/me/mtte/code/ideahub/model/Note.java
+++ backend/src/main/java/me/mtte/code/ideahub/model/Note.java
@@ -7,18 +7,18 @@ import java.sql.Timestamp;
 public class Note {
 
     private final int id;
-    private final int author;
     private final String title;
     private final String content;
     private final Timestamp created;
     private final boolean isShared;
+    private String author;
 
     public Note(NoteRecord noteRecord) {
-        this(noteRecord.getId(), noteRecord.getAuthor(), noteRecord.getTitle(), noteRecord.getContent(), noteRecord.getCreated(), noteRecord.getShared());
+        this(noteRecord.getId(), null, noteRecord.getTitle(), noteRecord.getContent(), noteRecord.getCreated(), noteRecord.getShared());
     }
 
 
-    public Note(int id, int author, String title, String content, Timestamp created, boolean isShared) {
+    public Note(int id, String author, String title, String content, Timestamp created, boolean isShared) {
         this.id = id;
         this.author = author;
         this.title = title;
@@ -31,7 +31,7 @@ public class Note {
         return id;
     }
 
-    public int getAuthor() {
+    public String getAuthor() {
         return author;
     }
 
